f6f99e17ec084f5a8caec76cf0362a1c28eeca67
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-08-27 11:38:07 +0100

    geral update

--- cmd/tools/terndotenv/main.go
+++ cmd/tools/terndotenv/main.go
@@ -1,25 +1,25 @@
-package main //mecanismo para organizar o código em módulos reutilizáveis
+package main
 
-import (//tem a função fundamental de incorporar funcionalidades de outros módulos ou bibliotecas em seu código
-	"os/exec" // Importa o pacote para executar comandos externos
-	
-	"github.com/joho/godotenv" // Importa o pacote para carregar variáveis de ambiente
+import (
+	"os/exec"
+
+	"github.com/joho/godotenv"
 )
 
-func main() {//funcao para ponto de entrada do programa
-		if err := godotenv.Load(); err != nil { // Tenta carregar as variáveis de ambiente do arquivo .env
-			panic(err) // Se houver algum erro, o programa para com uma mensagem de erro
-		}
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
 
-		cmd := exec.Command( // Cria um novo comando para ser executado
-			"tern", // Nome do comando a ser executado (provavelmente uma ferramenta de migração)
-			"migrate", // Subcomando para executar a migração
-			"--migrations", // Opção para especificar o diretório das migrações
-			"./internal/store/pgstore/migrations", // Caminho para o diretório das migrações
-			"--config", // Opção para especificar o arquivo de configuração
-			"./internal/store/pgstore/migrations/tern.conf", // Caminho para o arquivo de configuração
-		)
-		if err := cmd.Run(); err != nil { // Executa o comando e verifica se houve algum erro
-			panic(err) // Se houver algum erro, o programa pára com uma mensagem de erro
-		}
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
 }
--- internal/store/pgstore/migrations/001_create_rooms_table.sql
+++ internal/store/pgstore/migrations/001_create_rooms_table.sql
@@ -6,4 +6,3 @@ CREATE TABLE IF NOT EXISTS rooms (
 ---- create above / drop below ----
 
 DROP TABLE IF EXISTS rooms;
-
